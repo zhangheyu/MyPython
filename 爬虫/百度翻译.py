1a3d8638da04f51ccf20f192808d98eaa2af7196
abner <502152574@qq.com>
2020-12-12 17:37:42 +0800

    简单的request库网页爬数据

---
+++ 爬虫/百度翻译.py
@@ -0,0 +1,24 @@
+import requests
+
+post_url = 'https://fanyi.baidu.com/sug'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36'
+}
+
+world = input('请输入要翻译的单词:')
+data = {
+    'kw': world
+}
+
+response = requests.post(url=post_url, data=data, headers=headers)
+# print(response.text)
+# print(response.headers['Content-Type'])
+resp_content_type = response.headers['Content-Type']
+
+if resp_content_type.find('json'):
+    dict_obj = response.json()
+    trans = dict_obj['data']
+else:
+    trans = response.text()
+
+print(f'单词 {world} 翻译后的内容是:\n {trans}')
