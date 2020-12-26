6cf8fe344e6471f40f874dd0aec4a3bf8f2e2a57
abner <502152574@qq.com>
2020-12-26 17:41:37 +0800

    scrapy简单的数据解析

--- 爬虫/scrapyStudy/scrapyStudy/spiders/first.py
+++ 爬虫/scrapyStudy/scrapyStudy/spiders/first.py
@@ -7,9 +7,29 @@ class FirstSpider(scrapy.Spider):
     # 允许的域名，用来限定start_urls列表中哪些url可以进行请求发送，一般不使用
     # allowed_domains = ['study.com']
     # 起始的url列表，该列表中的url会被scrapy自动进行请求的发送
-    start_urls = ['https://www.baidu.com', 'https://www.sogou.com/']
+    start_urls = ['https://www.qiushibaike.com/text/']
 
     # 用于数据解析，response返回请求成功后对应的数据
     def parse(self, response):
-        # pass
-        print(response)
+        # 解析作者的名字+段子内容
+        div_list = response.xpath('//*[@id="content"]/div/div[2]/div')
+        # print(len(div_list))
+        all_data = []  # 用于终端指令持久化存储parse返回的内容
+        for div in div_list:
+            # print(div)
+            # xpath返回的是列表，但列表类型一定是Selector类型的对象
+            # [<Selector xpath='./div[1]/a[2]/h2/text()' data='\n无书斋主\n'>]
+            # extract()可以将Selector对象的data存储的字符串提取出来
+            author = div.xpath('./div[1]/a[2]/h2/text()')[0].extract()
+            # 列表调用了extract()后，则表示将列表中每一个selector对象中data对应的字符串提取出来
+            content = div.xpath('./a[1]/div/span/text()').extract()
+            content = ''.join(content)
+            # print(f'作者：{author.strip()}\t段子：{content.strip()}')
+            data = {
+                '作者': author.strip(),
+                '段子': content.strip()
+            }
+            all_data.append(data)
+            print(data)
+        return all_data
+        # scrapy crawl first -o 段子.csv/json/xml/pickle/jl 存储parse返回的数据
