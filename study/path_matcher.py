1c97cdc769d10b4ec7be932e7eb9493c2bbe4111
heyu.zhang <heyu.zhang@riversecurity.com>
2021-06-29 10:20:36 +0800

    Update path_matcher.py
    format and debug

--- study/path_matcher.py
+++ study/path_matcher.py
@@ -1,11 +1,14 @@
 # keep python && lua version consistent
+# encoding=utf-8
+
 import json
 
-END = '{{END}}'  #{{}} not appear in user input
+END = '{{END}}'  # {{}} not appear in user input
 SUB_TREE = '{{SUB_TREE}}'
 MATCH_ANY = '{{ANY}}'
 
-PATH_MAX_DEPTH = 100 + 10  #presived 10 for host method protocol
+PATH_MAX_DEPTH = 100 + 10  # presived 10 for host method protocol
+
 
 # END = '$'
 # SUB_TREE = '*'
@@ -29,8 +32,8 @@ class PathWalker():
                 cur_idx += 1
                 continue
             # print(start_idx, cur_idx)
-            assert(start_idx != cur_idx)
-            
+            assert (start_idx != cur_idx)
+
             yield path[start_idx: cur_idx]
 
             # print('after yild')
@@ -38,6 +41,7 @@ class PathWalker():
             cur_idx += 1
 
         yield path[start_idx:]
+
 
 class PathMatcher():
 
@@ -52,7 +56,7 @@ class PathMatcher():
             # print('value:', val)
 
             if not path:
-                assert(False)
+                assert (False)
 
             tree = root
             if len(path.split('/')) > PATH_MAX_DEPTH:
@@ -93,17 +97,20 @@ class PathMatcher():
 
         ret = self.do_match_tree(self.root, path_segments)
 
-
         return ret.get(END) if ret else None
 
     def do_match_tree(self, tree, path_segments):
 
         if len(path_segments) == 0:
+            print('tree:', json.dumps(tree, indent=2))
             return tree
 
+        print('path_segments:', path_segments)
         segment = path_segments[0]
+        print('segment:', segment)
 
         if segment in tree:
+            print('recursion')
             ret = self.do_match_tree(tree[segment], path_segments[1:])
             if ret and END in ret:
                 return ret
@@ -118,106 +125,109 @@ class PathMatcher():
 
         return None
 
+
 def test_path_matcher():
     patterns = ["/", "/aa", "/aa/", "/bb/*", "/bb",
                 "/cc/{id}/profile", "/cc/df/profile", "/cc/de/*", "/dd/{id}", "/dd/{id}/", "/ee/aa/bb/"]
     path_dict = {}
     for pattern in patterns:
         path_dict[pattern] = pattern
-    print('path_dict', path_dict)
+    # print('path_dict', path_dict)
 
     matcher = PathMatcher(path_dict)
     print('matcher', matcher)
+    assert (matcher.match('/aa') == '/aa')
+    assert (matcher.match('/bb/abac') == '/bb/*')
+    assert (matcher.match('/cc/de/profile') == '/cc/de/*')
+    assert (matcher.match('/cc/123/profile') == '/cc/{id}/profile')
 
 
 def test_path_match():
-    assert(list(PathWalker("/cc/{id}/profile//").walk()) == ['/cc','/{id}', '/profile', '/', '/'])
-    assert(list(PathWalker("/cc/{id}/profile/*").walk()) == ['/cc','/{id}', '/profile', '/*'])
+    assert (list(PathWalker("/cc/{id}/profile//").walk()) == ['/cc', '/{id}', '/profile', '/', '/'])
+    assert (list(PathWalker("/cc/{id}/profile/*").walk()) == ['/cc', '/{id}', '/profile', '/*'])
 
-    patterns = ["/", "/aa", "/aa/", "/bb/*", "/bb", "/cc/{id}/profile", "/cc/df/profile", "/cc/de/*", "/dd/{id}", "/dd/{id}/", "/ee/aa/bb/"]
+    patterns = ["/", "/aa", "/aa/", "/bb/*", "/bb", "/cc/{id}/profile", "/cc/df/profile", "/cc/de/*", "/dd/{id}",
+                "/dd/{id}/", "/ee/aa/bb/"]
     path_dict = {}
     for pattern in patterns:
         path_dict[pattern] = pattern
     print(path_dict)
 
     matcher = PathMatcher(path_dict)
-    #print(matcher)
+    # print(matcher)
 
-    assert(matcher.match('/') == '/')
-    assert(matcher.match('/aa') == '/aa')
-    assert(matcher.match('/aa/') == '/aa/')
-    assert(matcher.match('/bb') == '/bb')
-    assert(matcher.match('/bb/') == '/bb/*')
-    assert(matcher.match('/bb/abac') == '/bb/*')
-    assert(matcher.match('/bb/abac/fsd') == '/bb/*')
-    assert(matcher.match('/cc/df/profile') == '/cc/df/profile')
-    assert(matcher.match('/cc/123/profile') == '/cc/{id}/profile')
-    assert(matcher.match('/cc/de/profile') == '/cc/de/*')
-    assert(matcher.match('/dd/123') == '/dd/{id}')
-    assert(matcher.match('/dd/123/') == '/dd/{id}/')
+    assert (matcher.match('/') == '/')
+    assert (matcher.match('/aa') == '/aa')
+    assert (matcher.match('/aa/') == '/aa/')
+    assert (matcher.match('/bb') == '/bb')
+    assert (matcher.match('/bb/') == '/bb/*')
+    assert (matcher.match('/bb/abac') == '/bb/*')
+    assert (matcher.match('/bb/abac/fsd') == '/bb/*')
+    assert (matcher.match('/cc/df/profile') == '/cc/df/profile')
+    assert (matcher.match('/cc/123/profile') == '/cc/{id}/profile')
+    assert (matcher.match('/cc/de/profile') == '/cc/de/*')
+    assert (matcher.match('/dd/123') == '/dd/{id}')
+    assert (matcher.match('/dd/123/') == '/dd/{id}/')
 
     matcher = PathMatcher({'10.10.65.21:80/GET/36/11/': 1,
                            '10.10.65.21:80/{}/36/11/': 2})
 
-    assert(matcher.match('10.10.65.21:80/GET/36/11/') == 1)
-    assert(matcher.match('10.10.65.21:80/POST/36/11/') == 2)
-
+    assert (matcher.match('10.10.65.21:80/GET/36/11/') == 1)
+    assert (matcher.match('10.10.65.21:80/POST/36/11/') == 2)
 
     matcher = PathMatcher({'10.10.65.21:80/GET/36/{}/': 1,
                            '10.10.65.21:80/{}/36/11/': 2})
 
-    assert(matcher.match('10.10.65.21:80/GET/36/11/') == 1)
-    assert(matcher.match('10.10.65.21:80/POST/36/11/') == 2)
-
+    assert (matcher.match('10.10.65.21:80/GET/36/11/') == 1)
+    assert (matcher.match('10.10.65.21:80/POST/36/11/') == 2)
 
     matcher = PathMatcher({'10.10.65.21:80/GET/36/{}/': 1,
                            '10.10.65.21:80/GET/{}/11/': 2})
 
-    assert(matcher.match('10.10.65.21:80/GET/36/11/') == 1)
-    assert(matcher.match('10.10.65.21:80/GET/38/11/') == 2)
-
+    assert (matcher.match('10.10.65.21:80/GET/36/11/') == 1)
+    assert (matcher.match('10.10.65.21:80/GET/38/11/') == 2)
 
     matcher = PathMatcher({'10.10.65.21:80/GET/*': 1,
                            '10.10.65.21:80/GET/{}/11/': 2})
 
-    assert(matcher.match('10.10.65.21:80/GET/36/') == 1)
-    assert(matcher.match('10.10.65.21:80/GET/36/11/') == 2)
-    assert(matcher.match('10.10.65.21:80/GET/36/11') == 1)
-    assert(matcher.match('10.10.65.21:80/GET/38/12/') == 1)
-
-
+    assert (matcher.match('10.10.65.21:80/GET/36/') == 1)
+    assert (matcher.match('10.10.65.21:80/GET/36/11/') == 2)
+    assert (matcher.match('10.10.65.21:80/GET/36/11') == 1)
+    assert (matcher.match('10.10.65.21:80/GET/38/12/') == 1)
 
     matcher = PathMatcher({'10.10.65.21:80/GET/36/*': 1,
                            '10.10.65.21:80/GET/{}/11/': 2})
 
-    assert(matcher.match('10.10.65.21:80/GET/36') == None)
-    assert(matcher.match('10.10.65.21:80/GET/36/') == 1)
-    assert(matcher.match('10.10.65.21:80/GET/36/11/') == 1)
-    assert(matcher.match('10.10.65.21:80/GET/38/11/') == 2)
-    assert(matcher.match('10.10.65.21:80/GET/38/11/aa') == None)
-    assert(matcher.match('10.10.65.21:80/GET/38/11') == None)
+    assert (matcher.match('10.10.65.21:80/GET/36') == None)
+    assert (matcher.match('10.10.65.21:80/GET/36/') == 1)
+    assert (matcher.match('10.10.65.21:80/GET/36/11/') == 1)
+    assert (matcher.match('10.10.65.21:80/GET/38/11/') == 2)
+    assert (matcher.match('10.10.65.21:80/GET/38/11/aa') == None)
+    assert (matcher.match('10.10.65.21:80/GET/38/11') == None)
 
     deep_path1 = '10.10.65.21:80/GET/' + '/'.join([str(i) for i in range(PATH_MAX_DEPTH - 10)])
-    deep_path2 = '10.10.65.21:80/GET/' + '/'.join([str(i) for i in range(PATH_MAX_DEPTH)]) #ignore deep > PATH_MAX_DEPTH
-    matcher = PathMatcher({deep_path1: 1, deep_path2:2})
-    
-    assert(matcher.match(deep_path1) == 1)
-    assert(matcher.match(deep_path2) == None)
+    deep_path2 = '10.10.65.21:80/GET/' + '/'.join(
+        [str(i) for i in range(PATH_MAX_DEPTH)])  # ignore deep > PATH_MAX_DEPTH
+    matcher = PathMatcher({deep_path1: 1, deep_path2: 2})
+
+    assert (matcher.match(deep_path1) == 1)
+    assert (matcher.match(deep_path2) == None)
 
     matcher = PathMatcher({'10.10.65.21:80/GET/36/{id}': 1})
-    
-    assert(matcher.match('10.10.65.21:80/GET/36/') == None)
-    assert(matcher.match('10.10.65.21:80/GET/36/3') == 1)
-    assert(matcher.match('10.10.65.21:80/GET/36/3/') == None)
-    
+
+    assert (matcher.match('10.10.65.21:80/GET/36/') == None)
+    assert (matcher.match('10.10.65.21:80/GET/36/3') == 1)
+    assert (matcher.match('10.10.65.21:80/GET/36/3/') == None)
+
     matcher = PathMatcher({'10.10.65.21:80/GET/36/{id}': 1,
                            '10.10.65.21:80/GET/36/{id}/*': 2})
-    
-    assert(matcher.match('10.10.65.21:80/GET/36/') == None)
-    assert(matcher.match('10.10.65.21:80/GET/36/3') == 1)
-    assert(matcher.match('10.10.65.21:80/GET/36/3/') == 2)
-    assert(matcher.match('10.10.65.21:80/GET/36/3/3') == 2)
+
+    assert (matcher.match('10.10.65.21:80/GET/36/') == None)
+    assert (matcher.match('10.10.65.21:80/GET/36/3') == 1)
+    assert (matcher.match('10.10.65.21:80/GET/36/3/') == 2)
+    assert (matcher.match('10.10.65.21:80/GET/36/3/3') == 2)
+
 
 if __name__ == '__main__':
-    test_path_match()
-    # test_path_matcher()
+    # test_path_match()
+    test_path_matcher()
