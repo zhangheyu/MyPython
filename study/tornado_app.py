f2a897b4325c1f21c0e3cff2846a4445c2c2874e
heyu.zhang <heyu.zhang@riversecurity.com>
2021-07-01 15:58:15 +0800

    Update tornado_app.py
    format

--- study/tornado_app.py
+++ study/tornado_app.py
@@ -1,35 +1,35 @@
 import tornado.ioloop
 import tornado.web
 
+
 class MainHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.write("Hello, world\n")
+    def get(self):
+        self.write("Hello, world\n")
 
-
-	def post(self):
-		self.set_header("Content-Type", "text/plain")
-		self.write("You wrote " + self.get_body_argument("message"))
+    def post(self):
+        self.set_header("Content-Type", "text/plain")
+        self.write("You wrote " + self.get_body_argument("message"))
 
 
 class SubHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.write("Hello, Sub request\n")
+    def get(self):
+        self.write("Hello, Sub request\n")
 
 
 def make_app():
-	"""
+    """
 	curl 127.0.0.1:8888
 	curl 127.0.0.1:8888/sub
 	curl 127.0.0.1:8888 -d message=1111
 	"""
 
-	return tornado.web.Application([
-					(r"/", MainHandler),
-					(r"/sub", SubHandler),
-					])
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/sub", SubHandler),
+    ])
 
 
 if __name__ == "__main__":
-	app = make_app()
-	app.listen(8888)
-	tornado.ioloop.IOLoop.current().start()
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
